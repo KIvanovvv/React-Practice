f6b1f40ee093e2c4b362e5b511e2774bf33dc5ec
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-12-02 18:49:57 +0200

    Adding Button component

--- 04-starting-project/src/components/User/AddUser.js
+++ 04-starting-project/src/components/User/AddUser.js
@@ -1,19 +1,25 @@
 import React from "react";
+import Button from "../UI/Button.js";
 import Card from "../UI/Card.js";
 
 import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const addUserHandler = (e) => {
     e.preventDefault();
+    const formData = new FormData(e.target);
+    const { username, age } = Object.fromEntries(formData);
+    console.log(username, age);
+    e.target.reset();
   };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" name="username" type="text" />
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <input id="age" type="number" name="age" />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
