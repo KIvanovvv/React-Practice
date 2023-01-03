1d37cddf8444ee13abd44e4ede08fcfe28bd4836
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-01-03 23:35:47 +0200

    Inputs practice

--- 11-WorkingWithFormsAndUserInput/src/components/SimpleInput.js
+++ 11-WorkingWithFormsAndUserInput/src/components/SimpleInput.js
@@ -1,9 +1,31 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  //const [name, setName] = useState("");
+  const inputNameRef = useRef();
+
+  const onInputChangeHandler = (e) => {
+    // setName(e.target.value);
+    setName(inputNameRef.current.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(name);
+    setName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={onSubmit}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          onChange={onInputChangeHandler}
+          type="text"
+          id="name"
+          ref={inputNameRef}
+          value={name}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
